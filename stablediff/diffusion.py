2633dd9e968dc17f1cb1ae8832e27cedf9ba224f
debarpan <debarpankole@gmail.com>
2026-02-02 19:02:21 +0530

    completed UNET layers

--- stablediff/diffusion.py
+++ stablediff/diffusion.py
@@ -1,7 +1,7 @@
 import torch
 from torch import nn
 from torch.nn import functional as F
-from attention import SelfAttention, CrossAttention
+from stablediff.attention import SelfAttention, CrossAttention
 
 
 class TimeEmbedding(nn.Module):
@@ -23,7 +23,7 @@ class TimeEmbedding(nn.Module):
         #(1, 1280)
         return x
     
-class UNET_ResidualBlock(nn.Module):
+class UNET_ResidualBlock(nn.Module): # we are relating the latent with the time embedding so that the output will depend on the combination of both
 
     def __init__(self, in_channels: int, out_channels: int, n_time = 1280):
         super().__init__()
@@ -34,7 +34,116 @@ class UNET_ResidualBlock(nn.Module):
         self.groupnorm_merged = nn.GroupNorm(32, out_channels)
         self.conv_merged = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
 
+        if in_channels ==  out_channels:
+            self.residual_layer = nn.Identity()
+        else:
+            self.residual_layer = nn.Conv2d(in_channels, out_channels, kernel_size=1, padding=0)
         
+    def forward(self, feature, time):
+        # feature: (batch_size, in_channels, height, width)
+        # time (1, 1280)
+
+        residue = feature
+
+        feature = self.groupnorm_feature(feature)
+
+        feature = F.silu(feature)
+
+        feature = self.conv_feature(feature)
+
+        time = F.silu(time)
+
+        time = self.linear_time(time)
+
+        merged = feature + time.unsqueeze(-1).unsqueeze(-1)
+
+        merged = self.groupnorm_feature(merged)
+
+        merged = F.silu(merged)
+
+        merged = self.conv_merged(merged)
+
+        return merged + self.residual_layer(residue)
+    
+
+class UNET_AttentionBlock(nn.Module):
+
+    def __init__(self, n_head: int, n_embed: int, d_context = 768):
+        super().__init__()
+        channels = n_head * n_embed
+
+        self.groupnorm = nn.GroupNorm(32, channels, eps=1e-6)
+        self.conv_input = nn.Conv2d(channels, channels, kernel_size=1, padding =0)
+
+        self.layernorm_1 = nn.LayerNorm(channels)
+        self.attention_1 = SelfAttention(n_head, channels, in_proj_bias=False)
+        self.layernorm_2 = nn.LayerNorm(channels)
+        self.attention_2 = CrossAttention(n_head, channels, d_context, in_proj_bias=False)
+        self.layernorm_3 = nn.LayerNorm(channels)
+        self.linear_geglu_1 = nn.Linear(channels, 4 * channels * 2)
+        self.linear_geglu_2 = nn.Linear(4 * channels, channels)
+
+        self.conv_output = nn.Conv2d(channels, channels, kernel_size=1, padding=0)
+
+    def forward(self, x, context):
+        # x: (batch_size, features, ht, width)
+        # context: (batch_size, seq_len, dim)
+
+        residue_long = x
+
+        x = self.groupnorm(x)
+
+        x = self.conv_input(x)
+
+        n, c, h, w = x.shape
+
+        # (batch_size, features, ht, width) -> (batch_size, features, ht * width)
+        x = x.view(n, c, h*w)
+
+        # (batch_size, features, ht * width) -> (batch_size, ht * width, features)
+        x = x.transpose(-1, -2)
+        
+        # normalization + self attention with skip connection
+
+        residue_short = x
+
+        x = self.layernorm_1(x)
+        self.attention_1(x)
+
+        x += residue_short
+
+        residue_short = x
+
+        #normalization + cross attention with skip connection
+        x = self.layernorm_2(x)
+
+        # cross attention
+        self.attention_2(x, context)
+
+        x += residue_short
+
+        residue_short = x
+
+        #normalization + FF with GeGLU and skip connection
+
+        x = self.layernorm_3(x)
+
+        x, gate = self.linear_geglu_1(x).chunk(2, dim=-1)
+        x = x * F.gelu(gate)
+
+        x = self.linear_geglu_2(x)
+
+        x += residue_short
+
+        #batch_size, ht * width, features -> batch_size, features, ht * width
+        x = x.transpose(-1, -2)
+
+        x = x.view(n, c, h, w)
+
+        return self.conv_output(x) + residue_long
+
+
+
 
 class Upsample(nn.Module):
 
